d516a591102f293aed5e7e8a3c360ff23ca54c7f
Quách Thành Long <stephensouth1307@gmail.com>
2025-08-04 16:31:53 +0700

    -update

--- Bai_1(4-8-2025)/web_sever/main.go
+++ Bai_1(4-8-2025)/web_sever/main.go
@@ -5,15 +5,38 @@ import (
 	"log"
 	"net/http"
 )
+func fromHandler(w http.ResponseWriter, r *http.Request) {
+	if err:= r.ParseForm(); err != nil {
+		fmt.Fprint(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprint(w, "Post request successful\n")
+	name := r.FormValue("name")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+func helloHandeler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method !="Get" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello, this is a web server using Go Lang")
+}
 func main() {
 	fileSever := http.FileServer(http.Dir("./web_sever"))
 	http.Handle("/", fileSever)
-	http.HandleFunc("/form",from)
+	http.HandleFunc("/form",fromHandler)
 	http.HandleFunc("/hello", helloHandeler)	
-	fmt.Println("Server is running on port 8080\n")
+	
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 		
 	}
 
 	}
+//D:\Code linh tinh\Go_Lang\Go_Full_Course\Bai_1(4-8-2025)\web_sever
