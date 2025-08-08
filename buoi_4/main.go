a8212d9217e9c119c12bd603c37f0799fc198e01
Quách Thành Long <stephensouth1307@gmail.com>
2025-08-08 13:26:51 +0700

    -update

---
+++ buoi_4/main.go
@@ -0,0 +1,18 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func swap(a, b *int) {
+	*a, *b = *b, *a
+}
+func main_buoi4() {
+	a := 5
+	b := 10
+	fmt.Println("Before swap:", a, b)
+	swap(&a, &b)
+	fmt.Println("After swap:", a, b)
+	fmt.Print("Swapping completed successfully.\n")
+}	
+
